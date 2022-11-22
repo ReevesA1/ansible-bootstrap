8dbe13eba0103f539365e4ddfd242836cf917bf0
ReevesA1 <adamrivard87@gmail.com>
2022-11-22 14:57:19 -0500

    only bootstrap

--- tasks/bootstrap/automated/build-snapd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Not using this file since I have paru build snapd with initial manual script with paru
-
-git clone https://aur.archlinux.org/snapd.git /home/rocket/Downloads/snapd/
-cd /home/rocket/Downloads/snapd
-makepkg -si --noconfirm 
