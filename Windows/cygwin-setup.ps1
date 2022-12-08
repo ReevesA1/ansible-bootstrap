7036c5b85cba4ed27292f7cc10ab1a31aee17063
ReevesA1 <adamrivard87@gmail.com>
2022-12-08 18:03:48 -0500

    d

--- Windows/cygwin-setup.ps1
+++ Windows/cygwin-setup.ps1
@@ -1,3 +1,5 @@
+# Used this git repo for reference https://github.com/AlexNabokikh/windows-playbook
+
 # Ensure PowerShell execution policy is set to RemoteSigned for the current user
 $ExecutionPolicy = Get-ExecutionPolicy -Scope CurrentUser
 if ($ExecutionPolicy -eq "RemoteSigned") {
@@ -18,3 +20,14 @@ else {
 }
 
 choco install cygwin --params "/InstallDir:C:\cygwin64 /DesktopIcon"
+
+cd C:\cygwin64
+
+./cygwinsetup.exe --quiet-mode --packages git,pip
+
+pip3 install ansible
+
+/usr/bin/python3.7 -m pip install --upgrade pip
+
+#ansible-galaxy collection install ansible.windows
+#ansible-galaxy collection install chocolatey.chocolatey
