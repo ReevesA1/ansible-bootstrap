57c7cd5d106e02d3f604b5e7d17158898471372b
ReevesA1 <adamrivard87@gmail.com>
2022-12-11 07:57:27 -0500

    f

--- Windows/2-run-in-cygwin.sh
+++ Windows/2-run-in-cygwin.sh
@@ -1,11 +1,16 @@
 cd C:\cygwin64
 
-./cygwinsetup.exe --quiet-mode --packages git,python3,python3-devel,python3-setuptools,gcc,make,python-pip-wheel,pip 
+./cygwinsetup.exe --quiet-mode --packages git,pip,wget 
+
+#python3-cffi
+#python3,python3-devel,python3-setuptools,gcc,make,python-pip-wheel
 
 
-/usr/bin/python3.9.exe -m pip install --upgrade pip
+#/usr/bin/python3.9.exe -m pip install --upgrade pip
 
-python3 -m pip install --user ansible
+#python3 -m pip install --user ansible
+
+pip3 install ansible
 
 
 
