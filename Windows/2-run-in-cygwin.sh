7b006ef6e89653eadc75620c2384c2889da6894d
ReevesA1 <adamrivard87@gmail.com>
2022-12-11 07:07:53 -0500

    f

--- Windows/2-run-in-cygwin.sh
+++ Windows/2-run-in-cygwin.sh
@@ -2,9 +2,11 @@ cd C:\cygwin64
 
 ./cygwinsetup.exe --quiet-mode --packages git,python3
 
+/usr/bin/python3.9.exe -m pip install --upgrade pip
+
 pip install ansible
 
-/usr/bin/python3.9.exe -m pip install --upgrade pip
+
 
 #ansible-galaxy collection install ansible.windows
 #ansible-galaxy collection install chocolatey.chocolatey
