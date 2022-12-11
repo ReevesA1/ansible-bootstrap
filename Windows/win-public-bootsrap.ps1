6017f4b15e119a3a0a26c7617aa9198c4543e4c0
ReevesA1 <adamrivard87@gmail.com>
2022-12-11 11:31:43 -0500

    d

--- Windows/win-public-bootsrap.ps1
+++ Windows/win-public-bootsrap.ps1
@@ -28,3 +28,126 @@ ForEach ($PackageName in $Packages)
 {
     choco install $PackageName -y
 }
+
+
+
+# Remove Bloat Packages
+
+$Bloatware = @(
+    #Unnecessary Windows 10 AppX Apps
+    "3DBuilder"
+    "Microsoft3DViewer"
+    "AppConnector"
+    "BingFinance"
+    "BingNews"
+    "BingSports"
+    "BingTranslator"
+    "BingWeather"
+    "BingFoodAndDrink"
+    "BingHealthAndFitness"
+    "BingTravel"
+    "MinecraftUWP"
+    "GamingServices"
+    # "WindowsReadingList"
+    "GetHelp"
+    "Getstarted"
+    "Messaging"
+    "Microsoft3DViewer"
+    "MicrosoftSolitaireCollection"
+    "NetworkSpeedTest"
+    "News"
+    "Lens"
+    "Sway"
+    "OneNote"
+    "OneConnect"
+    "People"
+    "Print3D"
+    "SkypeApp"
+    "Todos"
+    "Wallet"
+    "Whiteboard"
+    "WindowsAlarms"
+    "windowscommunicationsapps"
+    "WindowsFeedbackHub"
+    "WindowsMaps"
+    "WindowsPhone"
+    "WindowsSoundRecorder"
+    "ConnectivityStore"
+    "CommsPhone"
+    "ScreenSketch"
+    "Spotify"
+    "TCUI"
+    #"XboxApp"
+    #"XboxGameOverlay"
+    #"XboxGameCallableUI"
+    #"XboxSpeechToTextOverlay"
+    "MixedReality.Portal"
+    "ZuneMusic"
+    "ZuneVideo"
+    #"YourPhone"
+    "Getstarted"
+    "MicrosoftOfficeHub"
+
+    #Sponsored Windows 10 AppX Apps
+    #Add sponsored/featured apps to remove in the "*AppName*" format
+    "EclipseManager"
+    "ActiproSoftwareLLC"
+    "AdobeSystemsIncorporated.AdobePhotoshopExpress"
+    "Duolingo-LearnLanguagesforFree"
+    "PandoraMediaInc"
+    "CandyCrush"
+    "BubbleWitch3Saga"
+    "Wunderlist"
+    "Flipboard"
+    "Twitter"
+    "Facebook"
+    "Royal Revolt"
+    "Sway"
+    "Speed Test"
+    "Dolby"
+    "Viber"
+    "ACGMediaPlayer"
+    "Netflix"
+    "OneCalendar"
+    "LinkedInforWindows"
+    "HiddenCityMysteryofShadows"
+    "Hulu"
+    "HiddenCity"
+    "AdobePhotoshopExpress"
+    "HotspotShieldFreeVPN"
+
+    #Optional: Typically not removed but you can if you need to
+    "Advertising"
+    #"MSPaint"
+    #"MicrosoftStickyNotes"
+    #"Windows.Photos"
+    #"WindowsCalculator"
+    #"WindowsStore"
+
+    # HPBloatware Packages
+    "HPJumpStarts"
+    "HPPCHardwareDiagnosticsWindows"
+    "HPPowerManager"
+    "HPPrivacySettings"
+    "HPSupportAssistant"
+    "HPSureShieldAI"
+    "HPSystemInformation"
+    "HPQuickDrop"
+    "HPWorkWell"
+    "myHP"
+    "HPDesktopSupportUtilities"
+    "HPQuickTouch"
+    "HPEasyClean"
+    "HPSystemInformation"
+)
+
+Write-Host "Removing Bloatware"
+
+foreach ($Bloat in $Bloatware) {
+    Get-AppxPackage "*$Bloat*" | Remove-AppxPackage -ErrorAction SilentlyContinue
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$Bloat*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+    Write-Host "Trying to remove $Bloat."
+}
+
+
+Write-Output "Restart computer "
