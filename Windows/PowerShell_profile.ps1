e7ef023081de3bc287b0c1a1914188263597c408
ReevesA1 <adamrivard87@gmail.com>
2022-12-14 18:54:14 -0500

    d

--- Windows/PowerShell_profile.ps1
+++ Windows/PowerShell_profile.ps1
@@ -1,7 +1,18 @@
 #### Alias's
 
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+#View and Edit profile
+function pro {Notepad $PROFILE.CurrentUserAllHosts}
+
+# Update windows with PSWindowsUpdate Module
 function winup {Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\($env.computername-Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force}
 
+#Run ultimate-win-bootstrap.ps1
 function winult {$ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/ReevesA1/ansible-bootstrap/main/Windows/ultimate-win-bootstrap.ps1
 Invoke-Expression $($ScriptFromGithHub.Content)}
 
