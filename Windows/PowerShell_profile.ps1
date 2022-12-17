ce282c2de690ccc935efab9265f7508e53409317
ReevesA1 <adamrivard87@gmail.com>
2022-12-17 07:38:40 -0500

    d

---
+++ Windows/PowerShell_profile.ps1
@@ -0,0 +1,107 @@
+#### Alias's
+
+########################################################
+##           Basic Alias's & Functions                ##
+########################################################
+
+# view net-adapter
+Set-Alias -Name eth -Value get-netadapter
+
+
+#View and Edit profile
+#function pro {Notepad $PROFILE.CurrentUserAllHosts}
+function pro {Notepad $PROFILE.AllUsersAllHosts} # Use AllUsers instead of Current so all my commands work when Im Admin
+
+# Update windows with PSWindowsUpdate Module
+function winup {Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\($env.computername-Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force}
+
+#Run ultimate-win-bootstrap.ps1
+function winult {$ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/ReevesA1/ansible-bootstrap/main/Windows/ultimate-win-bootstrap.ps1
+Invoke-Expression $($ScriptFromGithHub.Content)}
+
+
+# Tab Cycle = Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Useful shortcuts for traversing directories
+function cd...  { cd ..\.. }
+function cd.... { cd ..\..\.. }
+
+# Compute file hashes - useful for checking successful downloads 
+function md5    { Get-FileHash -Algorithm MD5 $args }
+function sha1   { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+
+########################################################
+##                  Admin/become Sudo Stuff           ##
+########################################################
+
+
+# Variables to Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If so and the current host is a command line, then change to red color 
+# as warning to user that they are operating in an elevated context
+if (($host.Name -match "ConsoleHost") -and ($isAdmin))
+{
+     $host.UI.RawUI.BackgroundColor = "DarkRed"
+     $host.PrivateData.ErrorBackgroundColor = "White"
+     $host.PrivateData.ErrorForegroundColor = "DarkRed"
+     Clear-Host
+}
+
+
+
+
+# Set up command prompt and window title. Use UNIX-style convention for identifying 
+# whether user is elevated (root) or not. Window title shows current version of PowerShell
+# and appends [ADMIN] if appropriate for easy taskbar identification
+function prompt 
+{ 
+    if ($isAdmin) 
+    {
+        "[" + (Get-Location) + "] # " 
+    }
+    else 
+    {
+        "[" + (Get-Location) + "] $ "
+    }
+}
+
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
+if ($isAdmin)
+{
+    $Host.UI.RawUI.WindowTitle += " [ADMIN - Be Careful]"
+}
+
+
+# Simple function to start a new elevated process. If arguments are supplied then 
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell is started.
+function admin
+{
+    if ($args.Count -gt 0)
+    {   
+       $argList = "& '" + $args + "'"
+       Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+    }
+    else
+    {
+       Start-Process "$psHome\powershell.exe" -Verb runAs
+    }
+}
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+
+
+# We don't need these variables any more; they were just temporary variables to get to $isAdmin. 
+# Delete them to prevent cluttering up the user profile. 
+Remove-Variable identity
+Remove-Variable principal
