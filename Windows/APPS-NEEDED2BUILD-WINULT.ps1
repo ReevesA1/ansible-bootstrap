916fc6b2401e7d07b556fa9e0fe55caba5590c1c
ReevesA1 <adamrivard87@gmail.com>
2022-12-18 11:58:59 -0500

    d

--- Windows/APPS-NEEDED2BUILD-WINULT.ps1
+++ Windows/APPS-NEEDED2BUILD-WINULT.ps1
@@ -1,14 +1,4 @@
 
-##################################################################################
-# Ensure PowerShell execution policy is set to RemoteSigned for the current user #
-##################################################################################
-$ExecutionPolicy = Get-ExecutionPolicy -Scope CurrentUser
-if ($ExecutionPolicy -eq "RemoteSigned") {
-    Write-Verbose "Execution policy is already set to RemoteSigned for the current user, skipping..." -Verbose
-}
-else {
-    Write-Verbose "Setting execution policy to RemoteSigned for the current user..." -Verbose
-    Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 }
 ################################################################
 ##                  Ensure chocolatey installed               ##
