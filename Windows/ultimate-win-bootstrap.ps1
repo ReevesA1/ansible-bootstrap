aea937349a7dc45cc496afb035a3a77699cfdf7f
ReevesA1 <adamrivard87@gmail.com>
2022-12-18 12:53:11 -0500

    d

--- Windows/ultimate-win-bootstrap.ps1
+++ Windows/ultimate-win-bootstrap.ps1
@@ -2,41 +2,18 @@
 ################################################################
 ##                      Shared Function                       ##
 ################################################################
-Function Check-RunAsAdministrator()
-{
-    #Get current user context
-    $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-    
-    #Check user is running the script is member of Administrator Group
-    if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
-    {
-        Write-host "Script is running with Administrator privileges!"
-    }
-    else
-      {
-        #Create a new Elevated process to Start PowerShell
-        $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
 
-        # Specify the current script path and name as a parameter
-        $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
 
-        #Set the Process to elevated
-        $ElevatedProcess.Verb = "runas"
+function Check-RunAsAdministrator {
+  # Check if the current process has elevated privileges
+  $currentPrincipal = [System.Security.Principal.WindowsPrincipal] [System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $isElevated = $currentPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 
-        #Start the new elevated process
-        [System.Diagnostics.Process]::Start($ElevatedProcess)
-
-        #Exit from the current, unelevated, process
-#        Exit
-       
+  # If the current process is not elevated, create a new elevated process
+  if (-not $isElevated) {
+    Start-Process PowerShell -Verb runAs
   }
 }
-
-#The Commented line below is the line I add to scripts to check if Script is running with Elevated Privileges
-#Check-RunAsAdministrator
-
-#Place your script here.
-#write-host "Welcome"
 
 
 ################################################
