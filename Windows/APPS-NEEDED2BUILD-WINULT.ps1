1721b7459d1a6b29e78575ef7f68f77ae925bb56
ReevesA1 <adamrivard87@gmail.com>
2022-12-18 18:16:06 -0500

    d

--- Windows/APPS-NEEDED2BUILD-WINULT.ps1
+++ Windows/APPS-NEEDED2BUILD-WINULT.ps1
@@ -83,6 +83,9 @@ $WingetPackages = @(
 
 Write-Host "Restoring Classic Right CLick Menu"
 New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name "InprocServer32" -force -value ""       
+
+Write-Host "Restoring Default Windows 11 Right Click Menu Layout"
+Remove-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Recurse -Confirm:$false -Force
  
     
 
