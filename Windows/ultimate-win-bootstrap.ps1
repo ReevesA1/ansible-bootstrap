e7133813ff8f393ce83e99c48bf5dac7f1025482
ReevesA1 <adamrivard87@gmail.com>
2022-12-18 12:17:59 -0500

    f

--- Windows/ultimate-win-bootstrap.ps1
+++ Windows/ultimate-win-bootstrap.ps1
@@ -347,6 +347,45 @@ function debloating {
 
 
 ################################################################
+##                      Shared Function                       ##
+################################################################
+Function Check-RunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+
+       #Exit from the current, unelevated, process
+       Exit
+       
+  }
+}
+
+#Check Script is running with Elevated Privileges
+Check-RunAsAdministrator
+
+#Place your script here.
+write-host "Welcome"
+
+################################################################
 ##                      Get List of installed Apps            ##
 ################################################################
 
@@ -370,6 +409,13 @@ function finish {
   Write-Host
   Pause
 }
+
+
+
+
+
+
+
 
 #############################################################################################
 ################################      Backend of Menu        ################################
@@ -413,6 +459,7 @@ function menu {
               finish
           }
           if ($actions -eq 2) {
+              Check-RunAsAdministrator()
               execution_policy 
               finish
           }
