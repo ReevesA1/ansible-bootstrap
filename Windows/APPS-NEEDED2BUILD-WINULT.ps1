2c587fe206f8dc8ee007b3e06499e2a5899321a5
ReevesA1 <adamrivard87@gmail.com>
2022-12-19 17:02:54 -0500

    s

--- Windows/APPS-NEEDED2BUILD-WINULT.ps1
+++ Windows/APPS-NEEDED2BUILD-WINULT.ps1
@@ -31,6 +31,8 @@ Remove-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905b
 # How to string together? 
 winget upgrade --all
 Get-WindowsUpdate -install -MicrosoftUpdate -AcceptAll | Out-File -FilePath "$($env:USERPROFILE)\Desktop\MSUpdates.log" -Force
+#update Profile
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/ReevesA1/ansible-bootstrap/main/Windows/PowerShell_profile.ps1 -OutFile $PROFILE.CurrentUserAllHosts
 
 ################################################################
 ##               Restart                               ##
@@ -43,5 +45,7 @@ Restart-Computer
 ################################################################
 ##               change the user account control (UAC)                             ##
 ################################################################
+
+# TO hardcord disables normal mode all together allways in power shell
  Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLUA' -Value 0
  Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLUA' -Value 1
