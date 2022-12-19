19da88367fdca6851b2008cb0afc4ceb1ef55953
ReevesA1 <adamrivard87@gmail.com>
2022-12-19 11:13:53 -0500

    f

--- Windows/ultimate-win-bootstrap.ps1
+++ Windows/ultimate-win-bootstrap.ps1
@@ -22,8 +22,8 @@ function Check-RunAsAdministrator {
   $currentPrincipal = [System.Security.Principal.WindowsPrincipal] [System.Security.Principal.WindowsIdentity]::GetCurrent()
   $isElevated = $currentPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 
-  # If the current process is not elevated, create a new elevated process
-  if (-not $isElevated) {
+  # If the current process is not elevated, create a new elevated process (notice I use Pwsh for powershell 7 and up insteal of using just the word powershell for old powershell 5)
+  if (-not $isElevated) { 
     Start-Process Pwsh -Verb runAs -ArgumentList "-Command $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/ReevesA1/ansible-bootstrap/main/Windows/ultimate-win-bootstrap.ps1
     Invoke-Expression $($ScriptFromGithHub.Content)"
     exit
