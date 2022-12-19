4df4a2ed57cde893630c84e0564fbbd9e7217b72
ReevesA1 <adamrivard87@gmail.com>
2022-12-19 15:53:01 -0500

    f

--- Windows/ultimate-win-bootstrap.ps1
+++ Windows/ultimate-win-bootstrap.ps1
@@ -6,8 +6,8 @@
 
 
 ################################################
-##              Shared Functions              ###
-#################################################
+##              Shared Functions              ##
+################################################
 
 # ____  _                        _              _       _           _       
 #/ ___|| |__   __ _ _ __ ___  __| |            / \   __| |_ __ ___ (_)_ __  
@@ -21,10 +21,10 @@ function Check-RunAsAdministrator {
   # Check if the current process has elevated privileges
   $currentPrincipal = [System.Security.Principal.WindowsPrincipal] [System.Security.Principal.WindowsIdentity]::GetCurrent()
   $isElevated = $currentPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
-
+  $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/ReevesA1/ansible-bootstrap/main/Windows/ultimate-win-bootstrap.ps1
   # If the current process is not elevated, create a new elevated process (notice I use Pwsh for powershell 7 and up instead of using just the word powershell (for old powershell 5)
     if (-not $isElevated) { 
-      Start-Process Pwsh -Verb runAs # -ArgumentList "-Command "
+      Start-Process Pwsh -Verb runAs -ArgumentList "-Command {Invoke-Expression $($ScriptFromGithHub.Content)}"
     exit
   }
 }
@@ -902,7 +902,7 @@ function menu {
               finish
           }
           if ($actions -eq 99) {
-              Write-Host "test10" 
+              Write-Host "test9" 
               finish
           }
           menu
